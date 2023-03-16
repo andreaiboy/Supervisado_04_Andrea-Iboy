afe7b9bff7c36875b94548a3cbcae940768056a3
Andrea <andreagiboy@gmail.com>
2023-03-16 07:36:02 -0600

    Agregar archivos de proyecto.

---
+++ Supervisado_04_Andrea Iboy/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+Console.WriteLine("_______________________________________");
+Console.WriteLine("Andrea Gabriela Iboy Casados - 1076623");
+Console.WriteLine("_______________________________________");
+
+int n = 0;
+
+try
+{
+    Console.WriteLine("Debes ingresar un número mayor a 0: ");
+    n = Convert.ToInt32(Console.ReadLine());
+
+    if (n <= 0)
+    {
+        throw new Exception("El numero que se debe ingresar es mayor a 0: ");
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Invalido, intentalo de nuevo: " + ex.Message);
+    return;
+}
+
+int a = 0, b = 1, c = 0;
+int i = 0;
+
+while (i < n)
+{
+    Console.WriteLine("{0}: {1}", i + 1, a);
+    c = a + b;
+    a = b;
+    b = c;
+    i++;
+}
